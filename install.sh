36569f831dd33d0f457d5af558795d7d88932cd2
Nicolas Sicard <rashguir@gmail.com>
2017-02-23 23:30:42 +0100

    WIP

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# on a fresh lubuntu install on the openbox session
+
+apt-get update -y
+apt-get upgrade -y
+apt-get dist-upgrade -y
+
+apt-get install -y terminator elementary-icon-theme obmenu pcmanfm
